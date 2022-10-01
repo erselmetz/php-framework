0fb63d921a5bcebb8bbeb75daa9c101e680c72ea
erselmetz <magbanuaersel@gmail.com>
2022-10-01 15:06:35 +0800

    initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../app/init.php';
+
+$app = new App;
+
+// class Test{
+//     public function default($a='',$b=''){
+//         echo 'A = '.$a.'<br>';
+//         echo 'b = '.$b.'<br>';
+//     }
+// }
+
+// $test = new Test;
+
+// call_user_func_array([$test,'default'],['This is a','This is b']);
