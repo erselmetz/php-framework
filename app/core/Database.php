9b9fc79662682b33850823f78807d363ccccadf4
erselmetz <magbanuaersel@gmail.com>
2022-10-01 16:36:28 +0800

    Illuminate Eloquent added

---
+++ app/core/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule();
+
+$capsule->addConnection([
+    'driver'=>'mysql',
+    'host'=>'localhost',
+    'username'=>'root',
+    'password'=>'',
+    'database'=>'scinfuse',
+    'charset'=>'utf8',
+    'collition'=>'utf8_unicode_ci',
+    'prefix'=>'',
+
+]);
+
+$capsule->bootEloquent();
