316d5e3e14ad70fd7f401255ee93f6c0848cf533
erselmetz <magbanuaersel@gmail.com>
2022-10-31 14:12:47 +0800

    db info is added

--- config.php
+++ config.php
@@ -1,3 +1,14 @@
 <?php
 
-$RewriteBase = '/php-frameword/';
+$RewriteBase = '/php-framework/';
+
+$database = [
+    'driver'=> 'mysql',
+    'host'=> 'localhost',
+    'username'=> 'root',
+    'password'=> '',
+    'database'=> 'db_name',
+    'charset'=> 'utf8',
+    'collation'=> 'utf8_unicode_ci',
+    'prefix'=> '',
+];
