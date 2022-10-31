3d1a0468ad6b2cb00774ed1e1ddb60267617c54c
erselmetz <magbanuaersel@gmail.com>
2022-10-31 19:04:35 +0800

    init location moved

---
+++ core/init.php
@@ -0,0 +1,11 @@
+<?php
+
+// autoload
+// require_once 'vendor/autoload.php';
+
+// database
+// require_once 'core/Database.php';
+
+require_once "core/config.php";
+require_once 'core/App.php';
+require_once 'core/Controller.php';
