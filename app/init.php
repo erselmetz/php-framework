ddb3d5106a33f380802e1ec621c5b278cc0be0d9
erselmetz <magbanuaersel@gmail.com>
2022-10-31 14:14:30 +0800

    config is added

--- app/init.php
+++ app/init.php
@@ -1,9 +1,11 @@
 <?php
+require_once "config.php";
+
 // autoload
 // require_once 'vendor/autoload.php';
 
 // database
 // require_once 'core/Database.php';
 
-require_once 'core/App.php';
-require_once 'core/Controller.php';
+require_once 'app/core/App.php';
+require_once 'app/core/Controller.php';
