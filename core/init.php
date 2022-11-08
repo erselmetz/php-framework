ea9cdeda41f8d65a57df7edeabbfdc1450b6cc8c
Ersel Metz Magbanua <65773510+erselmetz@users.noreply.github.com>
2022-11-08 13:19:25 +0800

    to lower case

--- core/init.php
+++ core/init.php
@@ -3,11 +3,11 @@
 require_once "core/config.php";
 
 // database
-require_once 'core/Database.php';
+require_once 'core/database.php';
 
 // autoload model
 spl_autoload_register(function($classname){
-    require_once 'app/Models/'.$classname.'.php';
+    require_once 'app/models/'.$classname.'.php';
 });
 
 require_once 'core/app.php';
